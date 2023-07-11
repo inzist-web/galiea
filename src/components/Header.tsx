9527414a85350c5dafea2d7e1829c63e306b8a5a
son1s <fomin.ea2003@gmail.com>
2023-07-11 04:19:07 +0300

    feat: added header

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link"
+import '@/blocks/header.scss'
+
+export default function Header() {
+  return (
+    <header className="header">
+      <Link href="/" className="header__logo">
+        <img src="/logo.svg" alt="логотип" className="header__icon" />
+        <h2 className="header__name">Галиэя</h2>
+      </Link>
+      <nav className="header__navbar">
+        <Link href="/" className="header__link header__link_active">О нас</Link>
+        <Link href="" className="header__link">Услуги</Link>
+        <Link href="" className="header__link">Тарифы</Link>
+        <Link href="" className="header__link">Контакты</Link>
+      </nav>
+    </header>
+  )
+}
