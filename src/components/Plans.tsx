3ebbeae2edd6e370ef368de0967ac899bc6ebcf9
son1s <fomin.ea2003@gmail.com>
2023-07-16 06:11:28 +0300

    feat: completed plans section

--- src/components/Plans.tsx
+++ src/components/Plans.tsx
@@ -1,4 +1,9 @@
+import Image from 'next/image'
 import '@/blocks/plans.scss'
+import blueFolder from '@/images/blue-folder.png'
+import darkFolder from '@/images/dark-folder.png'
+import lightFolder from '@/images/light-folder.png'
+import wideFolder from '@/images/wide-folder.png'
 
 export default function Plans() {
   return (
@@ -6,35 +11,35 @@ export default function Plans() {
       <h2 className="plans__heading">Тарифы:</h2>
       <div className="plans__wrapper-cards">
         <div className="plans__card">
-          <h2 className="plans__heading">ООО на УСН*</h2>
-          <img src="" alt="" className="plans__image" />
+          <h3 className="plans__card-heading">ООО на УСН*</h3>
+          <Image src={darkFolder} alt="" className="plans__image" />
           <div className="plans__content-wrapper">
             <p className="plans__text">*упрощенная система налогообложения для ООО — один из вариантов налогообложения для небольших фирм</p>
             <p className="plans__price">от <span className="plans__price_theme_bold">5000₽</span> в мес.</p>
           </div>
         </div>
         <div className="plans__card">
-          <h2 className="plans__heading">ООО на ОСН*</h2>
-          <img src="" alt="" className="plans__image" />
+          <h3 className="plans__card-heading">ООО на ОСН*</h3>
+          <Image src={blueFolder} alt="" className="plans__image" />
           <div className="plans__content-wrapper">
             <p className="plans__text">*общая система налогообложения — базовый режим для предпринимателей и компаний, заложенная в налоговом кодексе РФ</p>
             <p className="plans__price">от <span className="plans__price_theme_bold">7000₽</span> в мес.</p>
           </div>
         </div>
         <div className="plans__card">
-          <h2 className="plans__heading">ИП*</h2>
-          <img src="" alt="" className="plans__image" />
+          <h3 className="plans__card-heading">ИП*</h3>
+          <Image src={lightFolder} alt="" className="plans__image" />
           <div className="plans__content-wrapper">
             <p className="plans__text">*постоянный сбор и анализ информации из документов об активах и обязательствах, доходах и расходах индивидуального предпринимателя</p>
             <p className="plans__price">от <span className="plans__price_theme_bold">3000₽</span> в квартал</p>
           </div>
         </div>
-        <div className="plans__card">
+        <div className="plans__card plans__card_wide">
           <div className="plans__card-wrapper">
-            <h2 className="plans__heading">Регистрация ООО и ИП с открытием счета</h2>
-            <p className="plans__price plans__price_theme_blue">3500₽</p>
+            <h3 className="plans__card-heading plans__card-heading_wide">Регистрация ООО&nbsp;и&nbsp;ИП с&nbsp;открытием счета</h3>
+            <p className="plans__price plans__price_theme_blue plans__price_theme_bold">3500₽</p>
           </div>
-          <img src="" alt="" className="plans__image" />
+          <Image src={wideFolder} alt="" className="plans__image plans__image_wide" />
         </div>
         <div className="plans__card">
           <p className="plans__text plans__text_theme_blue">В каждом тарифе все пени и штрафы по нашей вине оплачиваем мы!</p>
