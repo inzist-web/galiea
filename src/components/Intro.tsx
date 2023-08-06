107f26de643cdcaae65915ec7f8474920d1fab9d
Kardit <mandrov.z.k@gmail.com>
2023-08-07 01:06:26 +0300

    fix: adaptives

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -16,13 +16,15 @@ export default function Intro() {
           <img src="/icons/send.svg" alt="" className="intro__link-icon"/>
         </Link>
       </div>
-      <div className="intro__image-composition">
-        <img src="/intro-bg.svg" alt="" className="intro__background" />
-        <h2 className="intro__image-text">Ваше спокойствие — наша забота</h2>
-        <img src="/logo.svg" alt="логотип" className="intro__logo" />
-        <Image src={office} alt="" className="intro__image_position_top" />
-        <Image src={writing} alt="" className="intro__image_position_left" />
-        <Image src={bridge} alt="" className="intro__image_position_right" />
+      <div className="intro__composition-container">
+        <div className="intro__image-composition">
+          <img src="/intro-bg.svg" alt="" className="intro__background" />
+          <h2 className="intro__image-text">Ваше спокойствие — наша забота</h2>
+          <img src="/logo.svg" alt="логотип" className="intro__logo" />
+          <Image src={office} alt="" className="intro__image_position_top" />
+          <Image src={writing} alt="" className="intro__image_position_left" />
+          <Image src={bridge} alt="" className="intro__image_position_right" />
+        </div>
       </div>
     </section>
   )
